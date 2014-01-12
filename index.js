1dcc2cd804c1980d0d198d8cdae8e5496cabdfd4
Jacob Gable <jacob.gable@gmail.com>
2014-01-12 12:31:32 -0600

    Make sure we match the original API for reporter use

--- index.js
+++ index.js
@@ -6,41 +6,37 @@ var fs = require('fs'),
 
 var jshintVersion = '0.2.4';
 
-var task = {
-    // Allow the original if needed.
-    original: jshintPlugin,
-    // Or, the cached version
-    cached: function (opt) {
-        var jshintOpts;
+// Add on to the original plugin
+jshintPlugin.cached = function (opt) {
+    var jshintOpts;
 
-        if (typeof opt === 'string') {
-            jshintOpts = fs.readFileSync(opt);
-        } else {
-            jshintOpts = JSON.stringify(opt);
-        } 
-        
-        return cache.proxy('jshint', {
-            task: jshintPlugin(opt),
-            key: function (file) {
-                return [file.contents.toString('utf8'), jshintVersion, jshintOpts].join('');
-            },
-            success: function (jshintedFile) {
-                return jshintedFile.jshint.success;
-            },
-            value: function (jshintedFile) {
-                var result = {
-                    jshint: {
-                        success: jshintedFile.jshint.success,
-                        cached: true
-                    }
-                };
-                return result;
-            }
-        });
-    }
+    if (typeof opt === 'string') {
+        jshintOpts = fs.readFileSync(opt);
+    } else {
+        jshintOpts = JSON.stringify(opt);
+    } 
+    
+    return cache.proxy('jshint', {
+        task: jshintPlugin(opt),
+        key: function (file) {
+            return [file.contents.toString('utf8'), jshintVersion, jshintOpts].join('');
+        },
+        success: function (jshintedFile) {
+            return jshintedFile.jshint.success;
+        },
+        value: function (jshintedFile) {
+            var result = {
+                jshint: {
+                    success: jshintedFile.jshint.success,
+                    cached: true
+                }
+            };
+            return result;
+        }
+    });
 };
 
 // Allow the fileCache to be modified externally
-task.cached.fileCache = cache.fileCache;
+jshintPlugin.cached.fileCache = cache.fileCache;
 
-module.exports = task;
+module.exports = jshintPlugin;
